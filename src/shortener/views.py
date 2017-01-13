3774352b1160b935f32897141569a4d430c8d7ea
shashishekhar1001 <shashishekhar1001@gmail.com>
2017-01-13 19:52:34 +0530

    Shortcode mapping through URL querying objects

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Shortener_URL 
 
 # Create your views here.
 def shortener_fbv(request, shortcode = None, *args, **kwargs):
     # print(args)
     # print(kwargs)
     print(shortcode)#Will print None if no arguement supplied
-    return render(request, "shortener.html", {"shortcode": shortcode})
+    obj_url = None
+    qs = Shortener_URL.objects.filter(shortcode__iexact=shortcode.upper())
+    if qs.exists() and qs.count() == 1:
+        obj_url = qs[0].url
+    
+    return render(request, "shortener.html", {"shortcode": obj_url})
