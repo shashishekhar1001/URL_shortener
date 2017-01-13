bc20fcb87d7840f52b72b935e82043388157c572
shashishekhar1001 <shashishekhar1001@gmail.com>
2017-01-13 14:32:19 +0530

    Arguement passed through URL

--- src/URL_Shortener/urls.py
+++ src/URL_Shortener/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from shortener.views import shortener_fbv
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^shortener/(?P<shortcode>[\w-]+)$', shortener_fbv),
 ]
--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def shortener_fbv(request, shortcode = None, *args, **kwargs):
+    # print(args)
+    # print(kwargs)
+    print(shortcode)#Will print None if no arguement supplied
+    return render(request, "shortener.html", {"shortcode": shortcode})
