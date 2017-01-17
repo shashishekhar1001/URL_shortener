6b1497c3277d1def4533db7a63c3af7f38d1233e
shashishekhar1001 <shashishekhar1001@gmail.com>
2017-01-17 18:16:31 +0530

    only view

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+from django.contrib.sites.shortcuts import get_current_site
+
 from .models import Shortener_URL 
 from .forms import SubmitUrlForm
 
@@ -18,7 +20,21 @@ def shortener_fbv(request, shortcode = None, *args, **kwargs):
 
 def home(request):
     form = SubmitUrlForm(request.POST or None)
+    context = {"form":form}
+    template = "home.html"
     if request.method == "POST":
         if form.is_valid():
-            pass
-    return render(request, "home.html", {"form":form})
+            new_url = "http://" + request.POST.get("url")
+            print(new_url)
+            obj, created = Shortener_URL.objects.get_or_create(url=new_url)
+            if created:
+                template = "success.html"
+                context = {"obj":obj, "created":created}
+            else:
+                template = "already-exists.html"
+                context = {"obj":obj, "created":created}                
+    return render(request, template, context)
+
+
+def test(request):
+    return render(request, "test.html", {})
